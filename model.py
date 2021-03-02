c54cf6f92e161d6bb4a72e118795694df5f57e05
namespace-Pt <zpt@ruc.edu.cn>
2021-03-02 13:41:56 +0800

    2021-3-2 batch version finished

--- model.py
+++ model.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn as nn
+START_TAG = '<START>'
+STOP_TAG = '<STOP>'
 
-START_TAG = 0
-STOP_TAG = 1
 class BiLSTM_CRF(nn.Module):
     '''
         BiLSTM with CRF for Named Entity Recognition
     '''
+
     def __init__(self, hparams, tag2idx):
         super().__init__()
 
@@ -40,7 +41,7 @@ class BiLSTM_CRF(nn.Module):
 
     def _lstm_encoder(self, sentence):
         """ encode sentence with BiLSTM
-        
+
         Args:
             sentence: word index sequence of [batch_size, seq_length]
 
@@ -53,22 +54,20 @@ class BiLSTM_CRF(nn.Module):
         lstm_out = lstm_out.view(len(sentence), self.batch_size, self.hidden_dim).transpose(0,1)
         lstm_feats = self.hidden2tag(lstm_out)
         return lstm_feats
-        
+
     def _forward_alg(self, feats):
         """ calculate the log-sum-exp of the score of all possible label sequence
 
         Args:
             feats: sentence embedding of [batch_size, seq_length, tagset_size]
-        
+
         Returns:
             alpha: batch of log-sum-exp of [batch_size, 1]
         """
 
-        # Do the forward algorithm to compute the partition function
         init_alphas = torch.full((self.batch_size, 1, self.tagset_size), -10000.)
         # START_TAG has all of the score.
         init_alphas[:, 0, self.tag2idx[START_TAG]] = 0.
-        # Wrap in a variable so that we will get automatic backprop
         forward_var = init_alphas
 
         # Iterate through the sentence
@@ -88,7 +87,7 @@ class BiLSTM_CRF(nn.Module):
 
         Args:
             feats: sentence embedding of [batch_size, seq_length, tagset_size]
-        
+
         Returns:
             path_score: batch of score of the input feats, of size [batch_size, 1]
             best_path: list of transmition trace
@@ -98,7 +97,6 @@ class BiLSTM_CRF(nn.Module):
         init_vvars = torch.full((self.batch_size, 1, self.tagset_size), -10000.)
         init_vvars[:, 0, self.tag2idx[START_TAG]] = 0.
 
-        # forward_var at step i holds the viterbi variables for step i-1
         forward_var = init_vvars
         for i in range(feats.shape[1]):
             feat = feats[:,i,:]
@@ -130,31 +128,27 @@ class BiLSTM_CRF(nn.Module):
         Args:
             feats: sentence embedding of [batch_size, seq_length, tagset_size]
             tags: label sequence of [batch_size, seq_length]
-        
+
         Returns:
             scores: batch of scores, size of [batch_size, 1]
         """
-        # Gives the score of a provided tag sequence
         score = torch.zeros((self.batch_size,1))
         tags = torch.cat([torch.full((self.batch_size, 1), self.tag2idx[START_TAG], dtype=torch.long), tags],dim=1)
         for i in range(feats.shape[0]):
             feat = feats[:,i,:]
             # calculate score in batch
             score = score + self.transitions[tags[:,i+1], tags[:,i]].view(self.batch_size,1) + feat.gather(dim=-1, index=tags[:,i].unsqueeze(dim=-1))
-        
+
         score = score + self.transitions[self.tag2idx[STOP_TAG], tags[:,-1]]
         return score
-    
+
     def neg_log_likelihood(self, sentence, tags):
         feats = self._lstm_encoder(sentence)
         forward_score = self._forward_alg(feats)
         gold_score = self._score_sentence(feats, tags)
         return forward_score - gold_score
 
-    def forward(self, sentence):  # dont confuse this with _forward_alg above.
-        # Get the emission scores from the BiLSTM
+    def forward(self, sentence):
         lstm_feats = self._lstm_encoder(sentence)
-
-        # Find the best path, given the features.
         score, tag_seq = self._viterbi_decode(lstm_feats)
         return score, tag_seq
